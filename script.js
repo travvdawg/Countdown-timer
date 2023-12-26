c815b4ad4371e08348b01dbac1f659cdece8ac78
Travis <124796989+travvdawg@users.noreply.github.com>
2023-12-25 21:46:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const seconds = document.getElementById('seconds');
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
-// console.log(newYearTime);
 
 const updateCountdown = () => {
 	const currentTime = new Date();
